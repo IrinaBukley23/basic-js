68fa8a94b7f65d8b208733c16fcea4d586b906c5
Irina <irenserge@mail.ru>
2023-12-24 11:31:10 +0300

    feat: add 3 tasks decision

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (type = true) {
+    this.alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    return this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    const alphabet = this.alphabet;
+    if(!message || !key) {
+      throw new Error ('Incorrect arguments!');
+    }
+    
+    let result = '';
+    let j = 0;
+    
+    for (let i = 0; i<message.length; i++) {
+        if (j > key.length-1) {
+            j = 0;
+        };
+        if (!alphabet.includes(message[i].toUpperCase())) {
+            result +=message[i];
+        } else {
+            result += alphabet[(alphabet.indexOf(message[i].toUpperCase()) + alphabet.indexOf(key[j].toUpperCase()) ) % 26]
+            j++
+        }
+    }
+    return this.type ? result : result.split('').reverse().join('');
+  }
+  decrypt(message, key) {
+    const alphabet = this.alphabet;
+    if(!message || !key) {
+      throw new Error ('Incorrect arguments!');
+    }
+    let result = '';
+    let j = 0;
+
+    for (let i = 0; i<message.length; i++) {
+      if (j > key.length-1) {
+        j = 0;
+      };
+
+      if (!alphabet.includes(message[i].toUpperCase())) {
+        result +=message[i];
+      } else {
+        if ((alphabet.indexOf(message[i].toUpperCase()) - alphabet.indexOf(key[j].toUpperCase()) >= 0)) {
+          result += alphabet[(alphabet.indexOf(message[i].toUpperCase()) - alphabet.indexOf(key[j].toUpperCase()) )]
+          j++
+        } else {
+          let index = (alphabet.indexOf(message[i].toUpperCase()) - alphabet.indexOf(key[j].toUpperCase())) + 26;
+          result += alphabet[index];
+          j++
+        }
+          
+      } 
+    }
+    return this.type ? result : result.split('').reverse().join('');
   }
 }
 
